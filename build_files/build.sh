e73a1802fa02a4517fe1c0e1b8de522cfee120f7
adri6412 <32240572+adri6412@users.noreply.github.com>
2026-03-17 15:12:50 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,13 +13,11 @@ set -ouex pipefail
 dnf5 install -y tmux 
 
 # Install MacTahoe-kde theme
-cd /tmp/
+mkdir /osx/
+chmod 755 /osx/
+cd /osx/
 wget https://file.adrianofrongillo.ovh/s/YBLwjxKJmLyNdSE/download/plasma6-macos-3.6.zip
 unzip plasma6-macos-3.6.zip
-cd plasma6-macos
-chmod +x install.sh
-./install.sh
-cd /
 
 
 # Use a COPR Example:
