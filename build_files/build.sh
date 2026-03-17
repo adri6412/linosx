0f31c4df3285cc11a0c2fb7668125b3701130426
adri6412 <32240572+adri6412@users.noreply.github.com>
2026-03-17 18:10:29 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,14 +12,6 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
-# Install MacTahoe-kde theme
-mkdir /osx/
-chmod 755 /osx/
-cd /osx/
-wget https://file.adrianofrongillo.ovh/s/YBLwjxKJmLyNdSE/download/plasma6-macos-3.6.zip
-unzip plasma6-macos-3.6.zip
-
-
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
